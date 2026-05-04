aaf1b302b8377f4f4f669874f8abb08448235cae
Zhekach <62952369+Zhekach@users.noreply.github.com>
2026-05-04 18:14:57 +0300

    add player movement base

---
+++ Assets/_game/Scripts/Textures/TextureTilingByScale.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(Renderer))]
+public class TextureTilingByScale : MonoBehaviour
+{
+    [SerializeField] private Vector2 _tilingMultiplier = Vector2.one;
+
+    private Renderer _renderer;
+    private Vector3 _lastScale;
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Application.isPlaying)
+            return;
+
+        if (transform.localScale == _lastScale)
+            return;
+
+        UpdateTiling();
+    }
+
+    private void OnValidate()
+    {
+        Init();
+        UpdateTiling();
+    }
+#endif
+
+    private void Awake()
+    {
+        Init();
+
+#if UNITY_EDITOR
+        if (Application.isPlaying == false)
+            UpdateTiling();
+#endif
+    }
+
+    private void Init()
+    {
+        if (_renderer == null)
+            _renderer = GetComponent<Renderer>();
+    }
+
+    private void UpdateTiling()
+    {
+        if (_renderer == null)
+            return;
+
+        Vector3 scale = transform.localScale;
+
+        _renderer.sharedMaterial.mainTextureScale = new Vector2(
+            scale.x * _tilingMultiplier.x,
+            scale.z * _tilingMultiplier.y
+        );
+
+        _lastScale = scale;
+    }
+}
